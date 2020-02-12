b17b44db20e00dc4d57e6d6ea48be39f98f9c6b7
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-12 17:40:02 +0100

    implement data binding and events

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Cocido', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
-    new Recipe('Cocido', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
-    new Recipe('Cocido', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
-    new Recipe('Cocido', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
-    new Recipe('Cocido', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg')
+    new Recipe('Cocido2', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
+    new Recipe('Cocido3', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
+    new Recipe('Cocido4', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg'),
+    new Recipe('Cocido5', 'cocidooooooooorrrrssss', 'https://i.blogs.es/d67ec6/cocido_madrileno-copia/450_1000.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
